23378f541959bf8eb2e3da0a5afc3533cc9ab4a5
hjc3116 <45302006+hjc3116@users.noreply.github.com>
2021-10-04 13:20:20 +0800

    Create 2021_10_4.c

--- 2021_10_4.c
+++ 2021_10_4.c
@@ -2,18 +2,32 @@
 
 int main()
 {
+/*
 	int x;
 	int n = 0;
 	
 	scanf_s("%d", &x);
-	
 	while (x > 0) {
 		n++;
 		x = x / 10;
 	}
-	// printf("x=%d,n=%d",x.n)   调试 注意 特殊值：10的倍数，0 ，负数
+							// printf("x=%d,n=%d",x.n)   调试 注意 特殊值：10的倍数，0 ，负数
 	printf("%d", n);
+*/
+	
+/*
+	int x;
+	int y = 0;
 
+	scanf_s("%d",&x); 
+	int t = x; 				//使用另一个变量保存原始值
+	while( x > 1 ) {
+		x /= 2;
+		y ++;
+	}
+	printf("log2 of %d is %d.",t,y)
+*/
+	
 	return 0;
 }
 

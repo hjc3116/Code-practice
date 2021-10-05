0309c3cd314d9cb7a6b32b94d3a6273ec20e4fe0
hjc3116 <45302006+hjc3116@users.noreply.github.com>
2021-10-05 12:18:24 +0800

    Add files via upload

---
+++ 2021_10_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	int n = 0;
+	scanf_s("%d", &n);
+
+	int i = 0;
+	int fact = 1;
+
+	for (i = n; i >= 1; i--) {            ********** 也可改为 for( i = 2 ; i >=n ; i++){ fact *= i }
+																	
+		fact *= i;
+	}
+	printf("%d!=%d", n, fact);
+	*/                                
+
+	return 0;
+}
+

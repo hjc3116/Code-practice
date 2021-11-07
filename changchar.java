17ba98e969cf4d20e215800ed3ab9ad84d4b5326
hjc3116 <45302006+hjc3116@users.noreply.github.com>
2021-11-07 23:42:15 +0800

    Add files via upload

---
+++ changchar.java
@@ -0,0 +1,15 @@
+package com.bao;
+
+public class changchar {
+    public static void main(String[] args){
+        System.out.println("南京\t合肥\t广州");
+        System.out.println("南京\n合肥\n广州");
+        System.out.println("南京\r合肥\r广州");
+        System.out.println("南京\\合肥\\广州");
+        System.out.println("南京\"合肥\"广州");
+        System.out.println("南京\t 合肥\t 广州");
+        System.out.println("三国\t 罗贯中\t 1000");
+
+
+    }
+}

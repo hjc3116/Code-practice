cdf854e4491b17c404d21f30a5d0cf7f0fabca1b
hjc3116 <45302006+hjc3116@users.noreply.github.com>
2021-11-13 20:54:47 +0800

    Add files via upload

--- fangfa.java
+++ fangfa.java
@@ -1,25 +1,24 @@
 package lydx.bao;
 
+class Person {
+String name;
+int age;
+}
 public class fangfa {
-    public static void main(String[]args){
-
-        fangfa p1 = new fangfa();
-        p1.math();
-        p1.math02(10);//调用math02函数，并赋值10给他
-
-        p1.getsum(10,20);//调用setsum函数，同时赋值 num1=10，num2=20;num
-        int returnsum = p1.getsum(10,20);//把函数getsum 返回的值，赋给变量returnsum
-        System.out.println("返回的值 = " + returnsum);
-    }
-
-    public void math() {
+/**求和*/
+    private void math() {
         int sum = 0;
-        for(int i = 1 ; i <= 1000 ; i ++ ){
+        int yiqian = 1000;
+        for (int i = 1; i <= yiqian; i++) {
             sum += i;
         }
         System.out.println("result = " + sum);
     }
+    public void getmath(){
+        this.math();
+    }
 
+/**求前n项之和*/
     public void math02(int n){
         //（int n） 接收一个int参数，用户可以输入
         int sum = 0;
@@ -29,10 +28,70 @@ public class fangfa {
         System.out.println("result02 = " + sum);
     }
 
+/**求2数之和，返回一个指*/
     public int getsum(int num1, int num2){
         //可接收多个参数
         int sum = num1 + num2;
         return sum;
     }
+
+/**求和求差，返回2个值*/
+    public int[] getsum1(int n1, int n2){
+
+        int[] resarr = new int[2];
+        resarr[0] = n1 + n2;
+        resarr[1] = n1 - n2;
+        return resarr;
+
+    }
+
+/**判断一个数的奇偶*/
+    public boolean jugment(int n1){
+
+//        方法1：
+//        if(n1%2 == 0){
+//            return true;
+//        }else{
+//            return false;
+//        }
+
+//        方法2：
+//        return n1 % 2 !=0 ? true: false;
+        return n1 % 2  ==0;
+    }
+
+/**循环打印某char类型数据*/
+    public void print(int row , int col, char c){
+        for(int i = 0;i < row; i++){
+            for(int j = 0;j < col; j++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+    }
+
+/**方法的参数调用(指针)*/
+    public void test(Person test){
+    //注意与public void test (int test )区别
+        test.age = 1000;
+    }
+
+/** 递归，求第n个斐波那契数的值  1,1,2,3,5,6,13.。。。。。。n*/
+    public int fbnqs(int n) {
+        if (n >= 1) {
+            if (n == 1 || n == 2) {
+                return 1;
+            } else {
+                return fbnqs(n - 1) + fbnqs(n - 2);
+            }
+        }else{
+            System.out.println("请输入大于1 的数");
+        }
+        return -1;
+    }
 }
+
+
+
 
